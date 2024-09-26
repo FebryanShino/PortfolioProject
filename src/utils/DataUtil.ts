1c2d9647edcbb42926a6945c6fd1a3ce36def2c7
FebryanS <venty779@gmail.com>
2024-09-26 17:10:10 +0700

    feat(): add data util

--- src/utils/DataUtil.ts
+++ src/utils/DataUtil.ts
@@ -1,8 +1,20 @@
-export function paginate(
-  array: any,
-  pageSize: number,
-  pageNumber: number,
-): any[] {
-  // human-readable page numbers usually start with 1, so we reduce 1 in the first argument
-  return array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
+type SortDirectionType = 'ASC' | 'DESC';
+
+export class DataUtil {
+  static sortData(data: any[], sortDirection: SortDirectionType): any[] {
+    const isAscending = sortDirection === 'ASC';
+    const currentData = data;
+
+    if (isAscending) {
+      currentData.sort((a, b) => a - b);
+    } else {
+      currentData.sort((a, b) => b - a);
+    }
+    return currentData;
+  }
+
+  static paginate(array: any, pageSize: number, pageNumber: number): any[] {
+    // human-readable page numbers usually start with 1, so we reduce 1 in the first argument
+    return array.slice((pageNumber - 1) * pageSize, pageNumber * pageSize);
+  }
 }
