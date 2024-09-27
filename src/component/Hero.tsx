801f5a07af869c4372fa5e5e76ac7ff6d0738fa6
FebryanS <venty779@gmail.com>
2024-09-27 19:20:32 +0700

    fix(): minor

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
 export default function Hero() {
-    return (
-        <div
-            style={{
-                backgroundColor: 'cyan',
-                width: '100%',
-                height: '100svh',
-                backgroundImage: 'url(/hero.png)',
-                backgroundSize: 'cover',
-            }}
-        >
-            Hero
-        </div>
-    );
+  return (
+    <div
+      style={{
+        backgroundColor: 'cyan',
+        width: '100%',
+        height: '100svh',
+        backgroundImage: 'url(/hero.png)',
+        backgroundSize: 'cover',
+      }}
+    ></div>
+  );
 }
