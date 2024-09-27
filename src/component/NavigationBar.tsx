7b65e5ce09ad854d8b7f066e1fbd77b67c59eef6
FebryanS <venty779@gmail.com>
2024-09-27 19:20:52 +0700

    feat(): animated navbar on scroll

--- src/component/NavigationBar.tsx
+++ src/component/NavigationBar.tsx
@@ -1,12 +1,37 @@
 import { MoonOutlined } from '@ant-design/icons';
+import { useGSAP } from '@gsap/react';
 import { Button, Flex, Space } from 'antd';
-import React from 'react';
+import gsap from 'gsap';
+import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function NavigationBar() {
+  const container = useRef(null);
   const navigate = useNavigate();
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useGSAP(
+    () => {
+      gsap.set(container.current, { yPercent: -100 });
+      const handleScroll = () => {
+        const scrollY = window.scrollY;
+
+        if (scrollY === 0) {
+          gsap.to(container.current, { yPercent: -100, duration: 0.5 });
+        } else {
+          gsap.to(container.current, { yPercent: 0, duration: 0.5 });
+        }
+      };
+
+      window.addEventListener('scroll', handleScroll);
+
+      return () => window.removeEventListener('scroll', handleScroll);
+    },
+    { scope: container },
+  );
   return (
     <Flex
+      className="relative"
       align="center"
       justify="space-between"
       style={{
@@ -19,6 +44,10 @@ export default function NavigationBar() {
         left: 0,
       }}
     >
+      <div
+        ref={container}
+        className="bg-white absolute w-full h-full ml-[-70px] z-[-1]"
+      ></div>
       Aqutan
       <Space>
         <Button type="text" ghost onClick={() => navigate('/creation')}>
