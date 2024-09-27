a3d2410ea330e409164c625fb97572a372f638e3
FebryanS <venty779@gmail.com>
2024-09-27 19:21:32 +0700

    fix(): minor

--- src/pages/BlenderCreationPage.tsx
+++ src/pages/BlenderCreationPage.tsx
@@ -1,19 +1,26 @@
 import { Button, Carousel, Flex, Splitter, Typography } from 'antd';
-import React from 'react';
+import React, { useRef } from 'react';
 import ContentWrapper from '../component/ContentWrapper';
 import ResponsiveGridWrapper from '../component/ResponsiveGridWrapper';
 import { useLocation } from 'react-router-dom';
+import { useGSAP } from '@gsap/react';
+
+import { ScrollTrigger } from 'gsap/all';
+import BannerCard from '../component/BannerCard';
 
 const { Title } = Typography;
 
 export default function BlenderCreationPage() {
   const location = useLocation();
+
   return (
     <div>
       <Carousel autoplay autoplaySpeed={3000}>
-        {['', '', '', '', ''].map((item: string) => (
-          <img src="/hero.png" />
-        ))}
+        {['/hero.png', '/room-right.png', '/room-bed.png'].map(
+          (item: string) => (
+            <img src={item} />
+          ),
+        )}
       </Carousel>
       <Flex className="bg-black p-8" justify="center">
         <Title level={1}>
@@ -46,20 +53,9 @@ export default function BlenderCreationPage() {
 
       <ContentWrapper>
         <Title level={1}>Browse</Title>
-        <Flex justify="center" vertical gap={5} className="mb-20">
+        <Flex justify="center" vertical gap={5} className="content mb-20">
           {['3D Assets', '3D Renders'].map((item: string) => (
-            <Flex
-              justify="flex-end"
-              align="flex-start"
-              vertical
-              style={{ backgroundImage: 'url(/hero.png)' }}
-              className="bg-cover bg-center w-full h-96 cursor-pointer px-20 py-10"
-            >
-              <Title level={1}>{item}</Title>
-              <Button ghost size="large">
-                Browse
-              </Button>
-            </Flex>
+            <BannerCard title={item} href="assets" key={item} />
           ))}
         </Flex>
       </ContentWrapper>
