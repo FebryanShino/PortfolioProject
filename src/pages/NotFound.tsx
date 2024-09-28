af581b1446d45ef3ad69c19025c1a0788cc3c68b
FebryanS <venty779@gmail.com>
2024-09-28 15:53:43 +0700

    feat(): add 404 not found page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,10 @@
+import { Empty, Flex } from 'antd';
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <Flex justify="center" align="center" className="w-svw h-svh">
+      <Empty />
+    </Flex>
+  );
+}
