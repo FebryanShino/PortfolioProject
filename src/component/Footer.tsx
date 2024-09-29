9e1572280231305036f318c47708bd4141710ad1
FebryanS <venty779@gmail.com>
2024-09-29 14:47:19 +0700

    fix(): footer layout

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,6 +1,7 @@
 import { Divider, Flex, Space, Typography } from 'antd';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FOOTER_LINKS } from '../config/footer';
 
 const { Title } = Typography;
 
@@ -8,23 +9,27 @@ export default function Footer() {
   return (
     <div className="bg-black w-full h-auto text-white p-20">
       <Flex wrap={false}>
-        <Flex className="w-[50%]">
-          <Title style={{ color: 'white' }}>FebryanShino</Title>
+        <Flex className="w-[50%]" align="flex-start">
+          <div
+            style={{
+              height: '100px',
+              width: '300px',
+              backgroundImage: 'url(/logo.png)',
+            }}
+            className="bg-cover bg-center"
+          ></div>
         </Flex>
-        <Flex gap="3rem" justify="space-between" className="w-full" wrap>
-          {Array(4)
-            .fill('')
-            .map((item: string, index: number) => (
-              <Space direction="vertical" className="text-left">
-                <Title style={{ color: 'white' }} level={4}>
-                  Section {index + 1}
-                </Title>
-                <Link to={index.toString()}>Lorem, ipsum dolor.</Link>
-                <Link to={index.toString()}>Lorem ipsum dolor sit amet.</Link>
-                <Link to={index.toString()}>Lorem, ipsum.</Link>
-                <Link to={index.toString()}>lorem</Link>
-              </Space>
-            ))}
+        <Flex gap="4rem" className="w-full" wrap>
+          {FOOTER_LINKS.map((link: any, index: number) => (
+            <Space direction="vertical" className="text-left">
+              <Title style={{ color: 'white' }} level={4}>
+                {link.header}
+              </Title>
+              {link.items.map((item: any) => (
+                <Link to={item.href}>{item.name}</Link>
+              ))}
+            </Space>
+          ))}
         </Flex>
       </Flex>
       <Divider className="border-white mt-20" />
---
+++ src/config/footer.ts
@@ -0,0 +1,20 @@
+export const FOOTER_LINKS = [
+  {
+    header: 'Creations',
+    items: [
+      { name: '3D Arts', href: '/creation/blender' },
+      { name: 'Illustrations', href: '/creation/illustration' },
+      { name: 'Programming', href: '/creation/programming' },
+      { name: 'Games', href: '/creation/game' },
+    ],
+  },
+  {
+    header: 'Website',
+    items: [
+      {
+        name: 'Repository',
+        href: 'https://github.com/FebryanShino/PortfolioProject',
+      },
+    ],
+  },
+];
