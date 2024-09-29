ea48ba5e15688dce3747a5c34b9f9d0d3a4d9993
FebryanS <venty779@gmail.com>
2024-09-29 14:50:29 +0700

    feat(): fetch data from github api

--- src/pages/programming/ProgrammingLandingPage.tsx
+++ src/pages/programming/ProgrammingLandingPage.tsx
@@ -9,26 +9,40 @@ import {
   Empty,
   Flex,
   List,
+  Skeleton,
   Space,
   Statistic,
   Typography,
 } from 'antd';
 import { Link, Navigate } from 'react-router-dom';
 import CountUp from 'react-countup';
+import { callAPI } from '../../config/api';
+import { GithubProfileResposeType } from '../../config/api/responseTypes';
 
 const { Title, Text, Paragraph } = Typography;
 
 export default function ProgrammingLandingPage() {
-  const [githubProfile, setGithubProfile] = useState<any>(null);
+  const [githubProfile, setGithubProfile] =
+    useState<GithubProfileResposeType>();
   const [githubRepos, setGithubRepos] = useState<any[]>([]);
+
+  async function fetchProfile() {
+    const data = await callAPI<GithubProfileResposeType>({
+      url: 'https://api.github.com/users/FebryanShino',
+      method: 'GET',
+    });
+    setGithubProfile(data);
+  }
+  async function fetchRepos() {
+    const data = await callAPI<any[]>({
+      url: 'https://api.github.com/users/FebryanShino/repos',
+      method: 'GET',
+    });
+    setGithubRepos(data);
+  }
   useEffect(() => {
-    setGithubProfile(GITHUB_PROFILE);
-    setGithubRepos(
-      GITHUB_REPOS.sort(
-        (a, b) =>
-          new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime(),
-      ),
-    );
+    fetchProfile();
+    fetchRepos();
   }, []);
   return (
     <ContentWrapper>
@@ -50,47 +64,55 @@ export default function ProgrammingLandingPage() {
               </Link>
             </Space>
           </Flex>
-          <Card>
-            <Flex justify="space-around">
-              <Statistic
-                title="Joined on"
-                value={new Date(githubProfile.created_at).toLocaleDateString()}
-                precision={2}
-              />
-              <Divider type="vertical" />
-              <Statistic
-                title="Number of Repos"
-                value={githubRepos.length}
-                precision={2}
-                formatter={(value) => <CountUp end={value as number} />}
-              />
-            </Flex>
-          </Card>
-          <Card
-            className="text-left"
-            title={<Title level={3}>Repositories</Title>}
-          >
-            <List
-              itemLayout="horizontal"
-              dataSource={githubRepos}
-              renderItem={(item, index) => (
-                <List.Item>
-                  <List.Item.Meta
-                    avatar={
-                      <Avatar
-                        src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
-                      />
-                    }
-                    title={<a href="https://ant.design">{item.name}</a>}
-                    description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+          {githubRepos ? (
+            <>
+              <Card>
+                <Flex justify="space-around">
+                  <Statistic
+                    title="Joined on"
+                    value={new Date(
+                      githubProfile.created_at,
+                    ).toLocaleDateString()}
+                    precision={2}
                   />
-                </List.Item>
-              )}
-            />
-          </Card>
+                  <Divider type="vertical" />
+                  <Statistic
+                    title="Number of Repos"
+                    value={githubRepos.length}
+                    precision={2}
+                    formatter={(value) => <CountUp end={value as number} />}
+                  />
+                </Flex>
+              </Card>
+              <Card
+                className="text-left"
+                title={<Title level={3}>Repositories</Title>}
+              >
+                <List
+                  itemLayout="horizontal"
+                  dataSource={githubRepos}
+                  renderItem={(item, index) => (
+                    <List.Item>
+                      <List.Item.Meta
+                        avatar={
+                          <Avatar
+                            src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`}
+                          />
+                        }
+                        title={<a href="https://ant.design">{item.name}</a>}
+                        description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                      />
+                    </List.Item>
+                  )}
+                />
+              </Card>
+            </>
+          ) : (
+            <Skeleton active />
+          )}
         </>
       ) : (
-        <Empty />
+        <Skeleton active />
       )}
     </ContentWrapper>
   );
