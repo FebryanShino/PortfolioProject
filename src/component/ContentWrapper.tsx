ce61795921f531ca778ec97700eef8a436dcce87
FebryanS <venty779@gmail.com>
2024-09-29 09:24:26 +0700

    fix(): content wrapper

--- src/component/ContentWrapper.tsx
+++ src/component/ContentWrapper.tsx
@@ -1,14 +1,23 @@
+import { Flex } from 'antd';
 import React from 'react';
 
-interface ContentWrapperProps extends React.ComponentPropsWithRef<'div'> {}
+interface ContentWrapperProps extends React.ComponentPropsWithRef<'div'> {
+  maxWidth?: string | number;
+  contentGap?: string | number;
+}
 
-export default function ContentWrapper({
-  children,
-  ...props
-}: ContentWrapperProps) {
+export default function ContentWrapper(props: ContentWrapperProps) {
   return (
-    <div className="mx-20 py-32" {...props}>
-      {children}
-    </div>
+    <Flex className="w-[100%] py-32" align="center" vertical {...props}>
+      <Flex
+        vertical
+        gap={props.contentGap ? props.contentGap : 20}
+        style={{
+          width: props.maxWidth ? props.maxWidth : '1140px',
+        }}
+      >
+        {props.children}
+      </Flex>
+    </Flex>
   );
 }
