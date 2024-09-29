1f49dca4428e8a360fe7b1d6b4b5668c5bbe5ff5
FebryanS <venty779@gmail.com>
2024-09-29 09:24:42 +0700

    fix(): adjust hero

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -1,6 +1,12 @@
+import { Button, Divider, Flex, Space, Typography } from 'antd';
 import React from 'react';
+import ContentWrapper from './ContentWrapper';
+import { useNavigate } from 'react-router-dom';
+
+const { Text, Title, Paragraph } = Typography;
 
 export default function Hero() {
+  const navigate = useNavigate();
   return (
     <div
       style={{
@@ -10,14 +16,44 @@ export default function Hero() {
         backgroundSize: 'cover',
       }}
     >
-      <div
-        className=" w-full h-full"
+      <Flex
+        className="h-full text-left"
         style={{
           background:
             'linear-gradient(180deg, hsla(0,0%,95%, .5), hsla(0,0%,95%, 1))',
           backdropFilter: 'blur(10px)',
         }}
-      ></div>
+        vertical
+        justify="center"
+        gap={0}
+      >
+        <ContentWrapper contentGap={0}>
+          <Flex vertical>
+            <Text>Good Morning,</Text>
+            <h3>This is</h3>
+            <Title level={1}>Febryan Shino</Title>
+          </Flex>
+          <Paragraph className="w-[50%]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            non incidunt odio! Quae quo obcaecati eius, provident vero
+            laudantium tempore qui quasi voluptate debitis maxime dolorum?
+            Suscipit at rem velit?
+          </Paragraph>
+          <Flex gap={0} vertical>
+            <Title level={5}>Specialties</Title>
+            <Text>3D arts</Text>
+            <Text>Frontend Development</Text>
+            <Text>Backend Development</Text>
+          </Flex>
+          <Button
+            className="w-32 bg-[#453f3c] text-white"
+            size="large"
+            onClick={() => navigate('about')}
+          >
+            Contact me
+          </Button>
+        </ContentWrapper>
+      </Flex>
     </div>
   );
 }
