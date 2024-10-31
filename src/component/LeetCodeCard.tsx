356ae16a565411f2558d506ef8b3325c7d9be529
FebryanS <venty779@gmail.com>
2024-10-31 08:57:03 +0700

    feat(): add leet code card component

---
+++ src/component/LeetCodeCard.tsx
@@ -0,0 +1,55 @@
+import { CircularProgress } from '@mui/joy';
+import { Card, Flex, Progress, Space, Typography } from 'antd';
+import React from 'react';
+
+interface LeetCodeCardInterface extends React.ComponentPropsWithoutRef<'div'> {
+  easy: number;
+  medium: number;
+  hard: number;
+  taskTotal: number;
+}
+
+export default function LeetCodeCard(props: LeetCodeCardInterface) {
+  return (
+    <Card className="w-full">
+      <Flex justify="space-between" gap={50}>
+        <CircularProgress
+          determinate
+          sx={{ '--CircularProgress-size': '20rem' }}
+          value={80}
+        >
+          <Typography.Title level={2}>
+            {props.easy + props.medium + props.hard}
+          </Typography.Title>
+          <Typography.Text>/{props.taskTotal}</Typography.Text>
+        </CircularProgress>
+        <Space direction="vertical" className="w-full text-left">
+          <Typography.Title level={2}>LeetCode Problems</Typography.Title>
+          <Typography.Paragraph>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
+            facilis exercitationem possimus cum repudiandae dolorum quis
+            voluptatum corporis expedita sunt!
+          </Typography.Paragraph>
+          <Space direction="vertical" className="w-full text-left">
+            <Space>
+              <Typography.Text>Hard</Typography.Text>
+            </Space>
+            <Progress percent={props.hard} size="small" strokeColor={'red'} />
+          </Space>
+          <Space direction="vertical" className="w-full text-left">
+            <Space>
+              <Typography.Text>Medium</Typography.Text>
+            </Space>
+            <Progress percent={40} size="small" strokeColor={'yellow'} />
+          </Space>
+          <Space direction="vertical" className="w-full text-left">
+            <Space>
+              <Typography.Text>Easy</Typography.Text>
+            </Space>
+            <Progress percent={80} size="small" strokeColor={'aquamarine'} />
+          </Space>
+        </Space>
+      </Flex>
+    </Card>
+  );
+}
