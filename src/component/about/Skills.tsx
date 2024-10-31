02e8bd9c52f64362aedf5c7827aaad8516220efd
FebryanS <venty779@gmail.com>
2024-10-31 08:56:43 +0700

    feat(): add skill card

---
+++ src/component/about/Skills.tsx
@@ -0,0 +1,40 @@
+import { Card, Progress, Space, Typography } from 'antd';
+import React from 'react';
+import ResponsiveGridWrapper from '../ResponsiveGridWrapper';
+import { SkillCategoryType, SKILLS } from '../../app.constants';
+
+const { Title, Paragraph, Text } = Typography;
+
+interface SkillInterface extends React.ComponentPropsWithoutRef<'div'> {
+  data: SkillCategoryType[];
+}
+
+export default function Skill(props: SkillInterface) {
+  return (
+    <Card>
+      <Title level={2}>My Skills</Title>
+      <ResponsiveGridWrapper minSize="30rem">
+        {props.data.map((skill) => (
+          <Card>
+            <Title level={5}>{skill.title}</Title>
+            <Space direction="vertical" className="w-full text-left">
+              {skill.data.map((item) => (
+                <Space direction="vertical" className="w-full text-left">
+                  <Space>
+                    {item.icon}
+                    <Text>{item.title}</Text>
+                  </Space>
+                  <Progress
+                    percent={item.percentage}
+                    size="small"
+                    strokeColor={item.color}
+                  />
+                </Space>
+              ))}
+            </Space>
+          </Card>
+        ))}
+      </ResponsiveGridWrapper>
+    </Card>
+  );
+}
