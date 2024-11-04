4d1a1e1ee31a58caa39ea2479d590b696d34a6f3
FebryanS <venty779@gmail.com>
2024-11-04 10:55:38 +0700

    fix(): change joy ui theme

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { HashRouter } from 'react-router-dom';
+import { CssVarsProvider } from '@mui/joy';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement,
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
-    <React.StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <HashRouter>
+      <CssVarsProvider theme={theme}>
+        <App />
+      </CssVarsProvider>
+    </HashRouter>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
