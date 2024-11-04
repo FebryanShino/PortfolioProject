22bb0f1d13769daf29e0eb2b0fbe1be21120e567
FebryanS <venty779@gmail.com>
2024-11-04 10:55:46 +0700

    feat(): create theme

---
+++ src/theme.ts
@@ -0,0 +1,24 @@
+import { extendTheme } from '@mui/joy/styles';
+
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          50: 'hsl(0,0%,0%)',
+          100: 'hsl(0,0%,0%)',
+          200: 'hsl(0,0%,0%)',
+          300: 'hsl(0,0%,0%)',
+          400: 'hsl(0,0%,0%)',
+          500: 'hsl(0,0%,0%)',
+          600: 'hsl(0,0%,0%)',
+          700: 'hsl(0,0%,0%)',
+          800: 'hsl(0,0%,0%)',
+          900: 'hsl(0,0%,0%)',
+        },
+      },
+    },
+  },
+});
+
+export default theme;
